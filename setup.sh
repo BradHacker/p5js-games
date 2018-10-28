7675daaa91780d9138375ed6a5d7ddc140d4ea02
BradHacker <harkerbd@gmail.com>
2018-10-28 18:37:13 -0400

    Fix mongo install

--- setup.sh
+++ setup.sh
@@ -12,13 +12,12 @@ echo "(5/8) Unzipping MongoDB..."
 tar -zxvf mongodb-linux-x86_64-ubuntu1804-4.0.3.tgz
 echo "(6/8) Installing MongoDB..."
 mkdir -p mongodb
-cp -R -n mongodb-linux-x86_64-ubuntu1804-4.0.3/ mongodb
+cp -R -n -a mongodb-linux-x86_64-ubuntu1804-4.0.3/. mongodb/
 export PATH=/app/backend/mongodb/bin:$PATH
 echo "(7/8) Init DB..."
 mkdir -p db
 echo "(8/8) Starting DB..."
 pwd
-# ./mongodb/bin/mongod
-sudo service mongod start
-cat /var/log/mongodb/mongod.log
-echo "-----\n\nSetup Complete! Running Server!\n\n-----"
+./mongodb/bin/mongod --dbpath db &
+jobs
+echo "-----Setup Complete! Running Server!-----"
