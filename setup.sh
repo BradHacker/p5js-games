1b19c074783f9a815e58daecf271be90eb3465f8
BradHacker <harkerbd@gmail.com>
2018-10-28 18:14:48 -0400

    Change MongoDb version

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@ echo "(3/8) Installing backend packages..."
 cd backend
 npm
 echo "(4/8) Downloading MongoDB..."
-curl -O https://fastdl.mongodb.org/osx/mongodb-osx-x86_64-3.0.15.tgz
+curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.3.tgz
 echo "(5/8) Unzipping MongoDB..."
-tar -zxvf mongodb-osx-x86_64-3.0.15.tgz
+tar -zxvf mongodb-linux-x86_64-ubuntu1804-4.0.3.tgz
 echo "(6/8) Installing MongoDB..."
 mkdir -p mongodb
-cp -R -n mongodb-osx-x86_64-3.0.15/ mongodb
+cp -R -n mongodb-linux-x86_64-ubuntu1804-4.0.3/ mongodb
 export PATH="$PATH$(pwd)/mongodb/bin"
 echo "(7/8) Init DB..."
 mkdir -p db
 echo "(8/8) Starting DB..."
-ls mongodb
-ls mongodb/bin
+pwd
 ./mongodb/bin/mongod
+sudo service mongod start
 echo "-----\n\nSetup Complete! Running Server!\n\n-----"
