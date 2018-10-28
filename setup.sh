69387e251d9acaf7aa8e8ec82e77e85d22fcb1fa
BradHacker <harkerbd@gmail.com>
2018-10-28 18:18:36 -0400

    Fix mongo path

--- setup.sh
+++ setup.sh
@@ -13,11 +13,12 @@ tar -zxvf mongodb-linux-x86_64-ubuntu1804-4.0.3.tgz
 echo "(6/8) Installing MongoDB..."
 mkdir -p mongodb
 cp -R -n mongodb-linux-x86_64-ubuntu1804-4.0.3/ mongodb
-export PATH="$PATH$(pwd)/mongodb/bin"
+export PATH=/app/backend/mongodb/bin:$PATH
 echo "(7/8) Init DB..."
 mkdir -p db
 echo "(8/8) Starting DB..."
 pwd
-./mongodb/bin/mongod
+# ./mongodb/bin/mongod
 sudo service mongod start
+cat /var/log/mongodb/mongod.log
 echo "-----\n\nSetup Complete! Running Server!\n\n-----"
